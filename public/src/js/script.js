ea41a59f786180461c2880ecf03e3eefa6c04b10
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-07 17:55:28 +0530

    updated

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,7 +1,9 @@
+// declare variables
 const btn = document.querySelector('#send_btn');
 const messageInput = document.querySelector('.text_send')
 const chatDiv = document.querySelector('.chat_div');
 
+// adding event listener to the send btn
 btn.addEventListener('click', (e) => {
     const message = messageInput.value;
 
@@ -15,7 +17,7 @@ btn.addEventListener('click', (e) => {
     chatDiv.scrollTop = chatDiv.scrollHeight;
 });
 
-
+// making the send btn click when enter key is pressed
 document.addEventListener('keypress', (e) => {
     if (event.keyCode === 13) btn.click();
 })
