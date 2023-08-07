0ec1612609ae781ccaf240ca457cbd31f15ec927
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-07 20:27:52 +0530

    added socket.io module

--- server.js
+++ server.js
@@ -3,16 +3,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const pathfile = path.join(__dirname, 'public');
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const pathfile = path.join(__dirname, 'public');
-
 app.get('/', (req, res) => {
-    res.sendFile(`${pathfile}/index.html`)
+    res.sendFile(`${pathfile}/index.html`);
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('message', (data) => {
+        // Handle incoming messages and broadcast to all connected clients
+        io.emit('message', data); // Broadcast the message to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 const port = process.env.PORT || 4000;
