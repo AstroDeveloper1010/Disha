6d1cfb9b2fcac6b99fff3b882738ea1042f23962
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-07 15:43:12 +0530

    added appendChild functionality to the chat box

---
+++ public/src/js/script.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('#send_btn');
+const messageInput = document.querySelector('.text_send')
+const chatDiv = document.querySelector('.chat_div');
+
+btn.addEventListener('click', (e) => {
+    let isTrue = true;
+    const message = messageInput.value;
+
+    const newMessage = document.createElement('p');
+    if (isTrue) {
+        newMessage.className = 'left'
+        isTrue = false
+    }
+    else {
+        newMessage.className = 'right'
+        isTrue = true
+    }
+    newMessage.innerText = message;
+
+    chatDiv.appendChild(newMessage);
+
+    messageInput.value = "";
+});
+
+document.addEventListener('keypress', (e) => {
+    if (event.keyCode === 13) btn.click();
+})
